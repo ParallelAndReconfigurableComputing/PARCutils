5128b807932541947a6dbe58f2ad8df45c9f3333
xhu685 <xhu685@08b7a59c-0210-0410-8289-8f00336d13cc>
2013-07-31 09:32:00 +0000

    move doc

---
+++ doc/tutorial/examples/eg1/WorkerThread.java
@@ -0,0 +1,29 @@
+ 
+import pi.ParIterator;
+
+public class WorkerThread extends Thread {
+		
+	private ParIterator<String> pi = null;
+	private int id = -1;
+
+	public WorkerThread(int id, ParIterator<String> pi) {
+		this.id = id;
+		this.pi = pi;
+	}
+
+	public void run() {
+		while (pi.hasNext()) {
+			String element = pi.next();
+			System.out.println("Hello from Thread "+id+", who got element: "+element);
+			
+			// slow down the threads (to illustrate the scheduling)
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("    Thread "+id+" has finished.");
+	}
+}
+
